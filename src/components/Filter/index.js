a7638e3645e5ab1dddc2d0598b8848977710049d
Iqboljon <iqboljon2003.yuldashev@gmail.com>
2023-05-13 23:53:02 +0500

    Added another lesson

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,27 +1,23 @@
 import React from 'react'
+import {Dropdown} from "antd";
 import { Container,Icons} from './style'
 import Button from "../Generics/Button"
 import Input from "../Generics/Input"
-import {Dropdown} from "antd";
-import 'antd/dist/reset.css';
 const Filter = () => {
-  const menu=(
-    <h1>Salom</h1>
-  )
+  const menu=<h1>Salom</h1>;
+  
   return (
     <Container>
       <Input icon={<Icons.Houses/>} placeholder={"Enter an address, neighborhood, city, or ZIP code"}/>
       <Dropdown
-      menu={{
-        menu
-      }}
+      menu={menu}
       placement="bottomRight"
       arrow={{
         pointAtCenter: true,
       }}
-    >
-      <Button type={"light"}><Icons.Setting/>Advanced</Button>
-    </Dropdown>
+      >
+      </Dropdown>
+      <><Button type={"light"}><Icons.Setting/>Advanced</Button></>
       <Button><Icons.Filter/>Search</Button>
     </Container>
   )
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RootContext from './context';
-import 'antd/dist/reset.css'
 import './index.css';
+import 'antd/dist/reset.css';
 import Root from './root';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
